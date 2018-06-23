18e1e2eac7d221f734d227f34107dceb7424a3f8
Sean Conrad <seanconrad123@gmail.com>
2018-06-23 05:34:59 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,12 +19,7 @@ def monopoly_with_second_tier
 	monopoly = {
 		:railroads => {
 			:pieces => 4,
-			:names => {
-				:reading_railroad => {},
-				:pennsylvania_railroad => {},
-				:b_and_o_railroad => {},
-				:shortline => {}
-			},
+			:names => {},
 			:rent_in_dollars => {}
 		}
 	}
